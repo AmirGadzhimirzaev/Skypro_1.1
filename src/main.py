0594a4fb3cbf8d129e6732fc1bfc772bf35e822e
Amir <Amir93@yandex.ru>
2025-01-02 01:43:04 +0100

    no.1: init

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import math
+import os
+
+path = os.getcwd()
+
+def get_sum(a, b) -> float:
+    """Функция, которая возвращает список чисел встречающихся в обоих ппринимаемых списках"""
+
+    return math.sqrt(a + b)
